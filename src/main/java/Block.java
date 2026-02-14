a8e731ed33d2e2785167804d89ab5e8941e2ef60
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-02-14 22:09:47 +0530

    Final code and it is running

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -1,30 +1,17 @@
 import java.awt.Image;
 
-/**
- * Block class representing a block in the game.
- * <p>
- * This class holds the properties of a block, including its position, size, and image representation.
- */
 public class Block {
-    public int x;
-    public int y;
-    public int width;
-    public int height;
-    public Image image;
+    public int x;        // x position of the block on screen
+    public int y;        // y position of the block on screen
+    public int width;    // width of the block
+    public int height;   // height of the block
+    public Image image;  // image used to display the block
 
-    /**
-     * Constructor for Block class.
-     * @param x The x-coordinate of the block.
-     * @param y The y-coordinate of the block.
-     * @param width The width of the block.
-     * @param height The height of the block.
-     * @param image The image representing the block.
-     */
     Block(int x, int y, int width, int height, Image image) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.image = image;
+        this.x = x;          // assign x coordinate
+        this.y = y;          // assign y coordinate
+        this.width = width;  // assign block width
+        this.height = height;// assign block height
+        this.image = image;  // assign block image
     }
 }
